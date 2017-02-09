64b4a35b1d84f85fa94ca7660cab4b7064be6c9c
Justin <justamnew@gmail.com>
2017-02-08 17:42:37 -0700

    Test

--- app/src/main/java/comsitejtproductionshome/google/httpssites/pokemoncardgame/MainActivity.java
+++ app/src/main/java/comsitejtproductionshome/google/httpssites/pokemoncardgame/MainActivity.java
@@ -2,6 +2,8 @@ package comsitejtproductionshome.google.httpssites.pokemoncardgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button coinFlip= (Button) findViewById(R.id.coin);
+        coinFlip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int choice= (int) (Math.random()*2);
+                if(choice==0){
+                    //Heads!
+                }
+                else{
+                    //Tails!
+                }
+            }
+        });
+
+
+
+
+
+
     }
 }
