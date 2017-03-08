4d2fceb1baf99ae490c6b40a196e462b57cfeec6
Justin <justamnew@gmail.com>
2017-03-08 15:43:45 -0700

    And this time with feeling!

--- app/src/main/java/comsitejtproductionshome/google/httpssites/pokemoncardgame/MainActivity.java
+++ app/src/main/java/comsitejtproductionshome/google/httpssites/pokemoncardgame/MainActivity.java
@@ -4,18 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ButtonControl bc=new ButtonControl();
+
+    ImageButton btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn= (ImageButton) findViewById(R.id.buttonCoinFlip);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                coinFlipButton();
+            }
+        });
     }
-        public void coinFlipButton(View v){
-            ((TextView)findViewById(R.id.txtButtonTest)).setText("coin flip button pushed");
+        public void coinFlipButton(){
+            Toast.makeText(getApplicationContext(), "Pressed.", Toast.LENGTH_LONG).show();
             bc.coinFlip();
         }
 
